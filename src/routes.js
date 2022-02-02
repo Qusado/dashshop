3a38a45216e19bd8ebda52f4490785a0b3f1be85
Qusado <73778368+Qusado@users.noreply.github.com>
2022-02-02 14:02:39 +0500

    change url method

--- src/routes.js
+++ src/routes.js
@@ -4,20 +4,15 @@ import { MainPage } from './pages/main.page.js';
 import {Constructor} from "./pages/Constructor.js";
 import {OnemaketPage} from "./pages/onemaket.page.js";
 import {Profile} from "./pages/profile.page";
-import {Lox} from "./components/Lox";
 import {Dd_modal} from "./components/dd_modal";
 
 
-export const useRoutes = isAuthenticated =>{
+export const useRoutes = () =>{
         return (
             <Switch>
                 <Route exact path="/" component={MainPage}/>
                 <Route exact path="/profile" component={Profile}/>
-
-
-
                 <Route exact path="/dd_modal" component={Dd_modal}/>
-                <Route exact path="/lol" component={Lox}/>
                 <Route exact path="/constructor" component={Constructor}/>
                 <Route exact path="/one/:id_maket" component={OnemaketPage}/>
                     <Redirect to="/"/>
